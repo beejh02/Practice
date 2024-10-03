6767213678fecf7ae0023776bddcdec5ee8fb5bf
SiRyung <joon0448@naver.com>
2024-10-03 16:40:43 +0900

    updates

--- program1/src/router/index.js
+++ program1/src/router/index.js
@@ -1,17 +1,17 @@
-import { createWebHistory, createRouter } from 'vue-router'
-import PageTitle from '@/components/PageTitle.vue'
+// import { createWebHistory, createRouter } from 'vue-router'
+// import PageTitle from '@/components/PageTitle.vue'
 
-const routes = [
-    {
-        path: '/pagetitle',
-        name: 'PageTitle',
-        component: PageTitle
-    }
-]
+// const routes = [
+//     {
+//         path: '/pagetitle',
+//         name: 'PageTitle',
+//         component: PageTitle
+//     }
+// ]
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes
+// })
 
-export default router
+// export default router
