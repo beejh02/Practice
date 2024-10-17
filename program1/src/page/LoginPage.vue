23ec3a25d9eeb2bf595aacf8942939837ab65fb1
SiRyung <joon0448@naver.com>
2024-10-18 02:07:06 +0900

    updates

--- program1/src/page/LoginPage.vue
+++ program1/src/page/LoginPage.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="container">
-
-
+<div class="container">
         <!-- 사이트 로고 이미지 -->
         <div class="image-section">
             <img src="../assets/images/site-logo3.png" alt="이미지" />
@@ -12,6 +10,8 @@
         <div class="divider"></div>
         <div class="form-section">
             <h1>로그인하기</h1>
+
+
             <!-- Oauth 섹션 -->
             <div class="oauth">
                 <img src="../assets/images/임시사용이미지.png" class = "oauth-image" alt="OAuth로 로그인" @click="loginWithOAuth" style="cursor: pointer;"/>
@@ -19,14 +19,25 @@
                 <img src="../assets/images/임시사용이미지.png" class = "oauth-image" alt="OAuth로 로그인" @click="loginWithOAuth" style="cursor: pointer;"/>
             </div>
             <a class="login-message1">또는 이메일과 비밀번호로 로그인</a>
+
+
             <!-- 회원가입 & 로그인 입력 받기 -->
-            <form>
-                <label for="usermail">이메일</label>
-                <input type="text" id="usermail" name="usermail" required>
-                <label for="password">비밀번호</label>
-                <input type="password" id="password" name="password" required>
-                <button type="submit">로그인</button>
+            <form class = "login-container">
+                <div class="input-group">
+                    <label for="usermail">이메일<br></label>
+                    <input type="text" placeholder="Type here" id="usermail" name="usermail" required>
+                </div>
+                <div class="input-group">
+                    <label for="password">비밀번호<br></label>
+                    <input type="password" placeholder="**********" id="password" name="password" required>
+                </div>
             </form>
+
+
+            <div class="forget-and-login-button">
+                <button class="button-gradient" type="submit">로그인</button>
+            </div>
+            
         </div>
     </div>
 
