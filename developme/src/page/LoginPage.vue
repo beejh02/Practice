c9fd78e85e9221db499e57c5d031c0bd6f794d9e
SiRyung <joon0448@naver.com>
2024-11-13 00:47:59 +0900

    updates

--- developme/src/page/LoginPage.vue
+++ developme/src/page/LoginPage.vue
@@ -1,79 +1,100 @@
 <template>
-    <div class="home">
-    <!-- 사이트 로고 이미지 -->
-    <div class="image-section">
-        <img src="../assets/images/site-logo3.png" alt="이미지" />
-    </div>
-
-    <!-- 회원가입 & 로그인 섹션 -->
-    <div class="divider"></div>
-        <div class="form-section">
-            <h1>회원 가입</h1>
-
-            <!-- OAuth 섹션 -->
-            <div class="oauth">
-                <button id="google_oauth_login" class="oauth_button" @click="loginWithOAuth">
-                    <img src="../assets/images/임시사용이미지.png" class="oauth-image" alt="Google OAuth로 로그인" />
+    <div class = "page">
+        <div class = "cover_image"><img id = "site_logo" src="../assets/images/site-logo3.png" alt="이미지" /></div>
+        <div class = "divider"></div>
+        <div class = "section">
+            <div id = "display_signup"><span class = "span_basis">계정이 없어요! <button id = "signup_button">회원가입!</button></span></div>
+            <h1 id = "display_main_message">로그인하기</h1>
+            <div class="oauth_section">
+                <!-- Kakao OAuth 버튼 -->
+                <button id="oauth_kakao_login" @click="loginWithKakao">
+                <img src="../assets/images/임시사용이미지.png" alt="Kakao OAuth로 로그인" />
                 </button>
-                <button id="kakao_oauth_login" class="oauth_button" @click="loginWithOAuth">
-                    <img src="../assets/images/임시사용이미지.png" class="oauth-image" alt="Kakao OAuth로 로그인" />
+                <!-- GitHub OAuth 버튼 -->
+                <button id="oauth_github_login" @click="loginWithGitHub">
+                <img src="../assets/images/임시사용이미지.png" alt="GitHub OAuth로 로그인" />
                 </button>
-                <button id="github_oauth_login" class="oauth_button" @click="loginWithOAuth">
-                    <img src="../assets/images/임시사용이미지.png" class="oauth-image" alt="GitHub OAuth로 로그인" />
+                <!-- Google OAuth 버튼 -->
+                <button id="oauth_google_login" @click="loginWithGoogle">
+                <img src="../assets/images/임시사용이미지.png" alt="Google OAuth로 로그인" />
                 </button>
             </div>
+            <span class = "span_basis">또는 이메일과 비밀번호로 로그인</span>
 
-            <span id="noti_login">또는 이메일과 비밀번호로 로그인</span>
+            <form @submit.prevent="handleLogin">
+                <div id="input_box">
+                <div class="input_group">
+                    <label class="noti_input" for="usermail">이메일</label>
+                    <input class="input_bar" type="text" id="usermail" v-model="email" placeholder="Type here" required />
+                </div>
+                <div class="input_group">
+                    <label class="noti_input" for="password">비밀번호</label>
+                    <input class="input_bar" type="password" id="password" v-model="password" placeholder="Type here" required />
+                </div>
+                </div>
 
-            <!-- 회원가입 폼 -->
-            <form class="signup-form">
-                <div class="form-row">
-                    <div class="input-group">
-                        <label for="usermail">이메일</label>
-                        <input type="text" id="usermail" name="usermail" placeholder="Type here" required />
-                    </div>
-                    <div class="input-group">
-                        <label for="password">비밀번호</label>
-                        <input type="password" id="password" name="password" placeholder="**********" required />
-                    </div>
-                </div>
-    
-                <div class="submit-button">
-                    <button class="button-gradient" type="submit">제출</button>
-                </div>
+                <button id="login_button" type="submit">로그인</button>
             </form>
+            
         </div>
+
+
     </div>
+
 </template>
-    
-    <script>
-    export default {
-        data() {
-        return {
-            isDropdownOpen: false,
-            selectedItem: '선택하세요',
-        };
-        },
-        methods: {
-        toggleDropdown() {
-            this.isDropdownOpen = !this.isDropdownOpen;
-        },
-        selectItem(item) {
-            this.selectedItem = item;
-            this.isDropdownOpen = false;
-        },
-        loginWithOAuth() {
-            // OAuth 로그인 기능 구현
-        }
-        },
+
+
+
+
+<script>
+export default {
+data() {
+    return {
+    email: '',
+    password: ''
     };
-    </script>
-    
-    
+},
+methods: {
+    loginWithKakao() {
+        const kakaoAuthUrl = 'https://kauth.kakao.com/oauth/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code';
+        window.location.href = kakaoAuthUrl;
+    },
+    loginWithGitHub() {
+        const githubAuthUrl = 'https://github.com/login/oauth/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&scope=SCOPE';
+        window.location.href = githubAuthUrl;
+    },
+    loginWithGoogle() {
+        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/auth?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code&scope=SCOPE';
+        window.location.href = googleAuthUrl;
+    },
+    handleLogin() {
+    // API 요청 보내기
+    fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: this.email, password: this.password })
+    })
+    .then(response => {
+        if (response.ok) {
+        return response.json();
+        } else {
+        throw new Error('로그인에 실패했습니다.');
+        }
+    })
+    .then(data => {
+        console.log('로그인 성공:', data);
+    })
+    .catch(error => {
+        console.error('오류:', error);
+        alert('로그인 중 오류가 발생했습니다.');
+    });
+    }
+}
+};
+</script>
 
 
 
-
-
-
-<style scoped src="./LoginPage.css"></style>
+<style scoped src="./MainTest.css"></style>
--- developme/src/page/MainTest.vue
+++ developme/src/page/MainTest.vue
@@ -1,51 +1,110 @@
 <template>
-    <div class = "page">
-        <div class = "cover_image"><img id = "site_logo" src="../assets/images/site-logo3.png" alt="이미지" /></div>
-        <div class = "divider"></div>
-        <div class = "section">
-            <div id = "display_signup"><span class = "span_basis">계정이 없어요! <button id = "signup_button">회원가입!</button></span></div>
-            <h1 id = "display_main_message">로그인하기</h1>
-            <div class = "oauth_section">
-                <button id = "oauth_google_login">
-                    <img src="../assets/images/임시사용이미지.png" alt="Google OAuth로 로그인" />
-                </button>
-                <button id = "oauth_google_login">
-                    <img src="../assets/images/임시사용이미지.png" alt="Google OAuth로 로그인" />
-                </button>
-                <button id = "oauth_google_login">
-                    <img src="../assets/images/임시사용이미지.png" alt="Google OAuth로 로그인" />
-                </button>
-            </div>
-            <span class = "span_basis">또는 이메일과 비밀번호로 로그인</span>
-
-            <form id="login_basis_form">
-                <div id = "input_box">
-                    <div class="input_group">
-                        <label class="noti_input" for="usermail">이메일</label>
-                        <input class="input_bar" type="text" id="usermail" name="usermail" placeholder="Type here" required />
-                    </div>
-                    <div class="input_group">
-                        <label class="noti_input" for="password">비밀번호</label>
-                        <input class="input_bar" type="text" id="password" name="password" placeholder="Type here" required />
-                    </div>
-                </div>
-                
-                <button class="login_button">로그인</button>
-            </form>
-            
-        
-        
-        
-        
-        
+    <header>
+        <div class = "maintitle">
+            <h1>Develop Me!</h1>
         </div>
-    
+        <nav>
+            <ul>
+                <li><a href="#">투두 리스트</a></li>
+                <li><a href="#">캘린더</a></li>
+                <li><a href="comunitypage">커뮤니티</a></li>
+                <li><a href="#">로드맵</a></li>
+                <li><a href="#">알고리즘</a></li>
+                <li><a href="mypage">마이페이지</a></li>
+            </ul>
+        </nav>
+        <div class = "myinfo">
+            <button @click = logout_function class = "logout_button">로그아웃</button>
+            <div class = "welcome_me">{{ 사용자명 }}님 환영합니다.</div>
+        </div>
+    </header>
 
+    <div class ="container">
+        ddd
     </div>
-
 </template>
 
+
+
+
 <script>
+export default {
+    data() {
+        return {
+            activeButton: null,
+            activePostState: null
+        };
+    },
+    methods: {
+        logout_function() {
+            fetch('/api/users/logout', { method: 'POST' })
+                .then(response => {
+                    if (response.ok) {
+                        this.$router.push('/logout');
+                    } else {
+                        alert('로그아웃에 실패했습니다.');
+                    }
+                })
+                .catch(error => {
+                    console.error('로그아웃 오류:', error);
+                    alert('로그아웃 중 오류가 발생했습니다.');
+                });
+        },
+        searchFunction() {
+            const query = document.getElementById('search').value;
+            console.log("검색어:", query);
+        },
+        toggleButton(buttonId) {
+            if (this.activeButton === buttonId) {
+                this.activeButton = null;
+            } else {
+                this.activeButton = buttonId;
+            }
+
+            const url = `/api/button-status?buttonId=${this.activeButton}`;
+
+            fetch(url, {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('서버 응답:', data);
+            })
+            .catch(error => {
+                console.error('서버 요청 오류:', error);
+            });
+        },
+        togglePostState(buttonIndex) {
+            if (this.activePostState === buttonIndex) {
+                this.activePostState = null;
+            } else {
+                this.activePostState = buttonIndex;
+            }
+        },
+        writePost() {
+            // API 요청
+            fetch('/api/community/posts', {
+                method: 'GET'
+            })
+            .then(response => {
+                if (response.ok) {
+                    // 페이지 전환
+                    this.$router.push('/write-post'); // 글 작성 페이지로 이동
+                } else {
+                    alert('페이지로 전환하는데 문제가 발생했습니다.');
+                }
+            })
+            .catch(error => {
+                console.error('API 요청 오류:', error);
+                alert('요청 중 오류가 발생했습니다.');
+            });
+        }
+    }
+};
 </script>
 
-<style scoped src="./MainTest.css"></style>
+
+
+
+
+<style scoped src="./CommunityPage.css"></style>
