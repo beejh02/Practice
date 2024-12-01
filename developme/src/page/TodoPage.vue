f94ddab9cd3135b30b4689fdeded5e50ef9370e3
SiRyung <joon0448@naver.com>
2024-12-01 23:46:42 +0900

    updates

--- developme/src/page/TodoPage.vue
+++ developme/src/page/TodoPage.vue
@@ -1,22 +1,22 @@
 <template>
     <header>
-    <div class="maintitle">
-        <h1>Develop Me!</h1>
-    </div>
-    <nav>
-        <ul>
-        <li><a href="#">투두 리스트</a></li>
-        <li><a href="calendarpage">캘린더</a></li>
-        <li><a href="comunitypage">커뮤니티</a></li>
-        <li><a href="#">로드맵</a></li>
-        <li><a href="#">알고리즘</a></li>
-        <li><a href="mypage">마이페이지</a></li>
-        </ul>
-    </nav>
-    <div class="myinfo">
-        <button @click="logout_function" class="logout_button">로그아웃</button>
-        <div class="welcome_me"><!--{{ users.nickname }}-->님 환영합니다.</div>
-    </div>
+        <div class="maintitle">
+            <h1>Develop Me!</h1>
+        </div>
+        <nav>
+            <ul>
+            <li><a href="#">투두 리스트</a></li>
+            <li><a href="calendarpage">캘린더</a></li>
+            <li><a href="comunitypage">커뮤니티</a></li>
+            <li><a href="#">로드맵</a></li>
+            <li><a href="#">알고리즘</a></li>
+            <li><a href="mypage">마이페이지</a></li>
+            </ul>
+        </nav>
+        <div class="myinfo">
+            <button @click="logout_function" class="logout_button">로그아웃</button>
+            <div class="welcome_me"><!--{{ users.nickname }}-->님 환영합니다.</div>
+        </div>
     </header>
 
 
